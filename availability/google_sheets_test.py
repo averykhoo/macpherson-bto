02c137010116507c25b8bc9281b18202a231daa1
Avery Khoo <averykhoo@gmail.com>
2021-09-07 23:12:21 +0800

    test sheet read api

--- availability/google_sheets_test.py
+++ availability/google_sheets_test.py
@@ -1,49 +1,62 @@
 import os
+import re
 
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1ahbAXvuamz2PB1COGx2dWjIV8BN75bqYL_KmgdHkWKk'
-SAMPLE_RANGE_NAME = 'Blk 95A!A8:L26'
+spreadsheet_id = '1ahbAXvuamz2PB1COGx2dWjIV8BN75bqYL_KmgdHkWKk'
+
+
+class Workbook:
+    def __init__(self):
+
+        """Shows basic usage of the Sheets API.
+        Prints values from a sample spreadsheet.
+        """
+        creds = None
+
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+
+        service = build('sheets', 'v4', credentials=creds)
+        self.sheet = service.spreadsheets()
+
+    def get_sheet_range(self, sheet_name, range_start, range_end):
+        assert re.fullmatch(r'[A-Z]+[0-9]+', range_start)
+        assert re.fullmatch(r'[A-Z]+[0-9]+', range_end)
+        result = self.sheet.values().get(spreadsheetId=spreadsheet_id,
+                                         range=f'{sheet_name}!{range_start}:{range_end}',
+                                         ).execute()
+        print(result.get('range'))
+        print(result.get('majorDimension'))
+        return result.get('values', [])
+
 
 if __name__ == '__main__':
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
 
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    service = build('sheets', 'v4', credentials=creds)
-
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME,
-                                ).execute()
-    values = result.get('values', [])
+    wb = Workbook()
+    values = wb.get_sheet_range('Blk 95A', 'A1', 'P29')
 
     if not values:
         print('No data found.')
