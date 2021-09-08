c8a4dacc57f71aaaf0f5552ba5bf519bc9ae6994
Avery Khoo <averykhoo@gmail.com>
2021-09-09 00:00:22 +0800

    bit more testing

--- availability/google_sheets_test.py
+++ availability/google_sheets_test.py
@@ -1,5 +1,6 @@
 import os
 import re
+from pprint import pprint
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -8,9 +9,27 @@ from googleapiclient.discovery import build
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+# Scope (https://developers.google.com/sheets/api/guides/authorizing)
+#   Meaning
+# https://www.googleapis.com/auth/spreadsheets.readonly
+#   Allows read-only access to the user's sheets and their properties.
+# https://www.googleapis.com/auth/spreadsheets
+#   Allows read/write access to the user's sheets and their properties.
+# https://www.googleapis.com/auth/drive.readonly
+#   Allows read-only access to the user's file metadata and file content.
+# https://www.googleapis.com/auth/drive.file
+#   Per-file access to files created or opened by the app.
+# https://www.googleapis.com/auth/drive
+#   Full, permissive scope to access all of a user's files. Request this scope only when it is strictly necessary.
 
-# The ID and range of a sample spreadsheet.
-spreadsheet_id = '1ahbAXvuamz2PB1COGx2dWjIV8BN75bqYL_KmgdHkWKk'
+# https://docs.google.com/spreadsheets/d/1ahbAXvuamz2PB1COGx2dWjIV8BN75bqYL_KmgdHkWKk/edit#gid=1211096710
+# spreadsheet_id = '1ahbAXvuamz2PB1COGx2dWjIV8BN75bqYL_KmgdHkWKk'
+
+# https://docs.google.com/spreadsheets/d/1Hx_oFmbRYRuek_eyVUyfz4_b9861mPhSBF1NHH9et70/edit#gid=1116371039
+spreadsheet_id = '1Hx_oFmbRYRuek_eyVUyfz4_b9861mPhSBF1NHH9et70'  # copy, so I don't break anything
+
+# https://docs.google.com/spreadsheets/d/1NeklzsZ_EZXz0W5eyPdRbZmGpNqIdAGJyIMVYO342oo/edit#gid=0
+# spreadsheet_id = '1NeklzsZ_EZXz0W5eyPdRbZmGpNqIdAGJyIMVYO342oo'  # random unused sheet
 
 
 class Workbook:
@@ -42,7 +61,7 @@ class Workbook:
         service = build('sheets', 'v4', credentials=creds)
         self.sheet = service.spreadsheets()
 
-    def get_sheet_range(self, sheet_name, range_start, range_end):
+    def get_sheet_range_values(self, sheet_name, range_start, range_end):
         assert re.fullmatch(r'[A-Z]+[0-9]+', range_start)
         assert re.fullmatch(r'[A-Z]+[0-9]+', range_end)
         result = self.sheet.values().get(spreadsheetId=spreadsheet_id,
@@ -52,14 +71,31 @@ class Workbook:
         print(result.get('majorDimension'))
         return result.get('values', [])
 
+    def get_sheet_range(self, sheet_name, range_start, range_end):
+        assert re.fullmatch(r'[A-Z]+[0-9]+', range_start)
+        assert re.fullmatch(r'[A-Z]+[0-9]+', range_end)
+        result = self.sheet.get(spreadsheetId=spreadsheet_id,
+                                ranges=[f'{sheet_name}!{range_start}:{range_end}'],
+                                includeGridData=True,
+                                ).execute()
+        print(result.get('range'))
+        print(result.get('majorDimension'))
+        return result.get('values', [])
+
 
 if __name__ == '__main__':
-
     wb = Workbook()
-    values = wb.get_sheet_range('Blk 95A', 'A1', 'P29')
 
+    # values = wb.get_sheet_range_values('Blk 95A', 'A1', 'P29')
+    values = wb.get_sheet_range_values('Blk 95A', 'A11', 'B12')
     if not values:
         print('No data found.')
     else:
         for row in values:
             print(row)
+
+    values = wb.sheet.get(spreadsheetId=spreadsheet_id,
+                          ranges=['Blk 95A!A11:B12'],
+                          includeGridData=True).execute()
+    # pprint(values['properties'])
+    pprint(values['sheets'])
