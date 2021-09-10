b99b765a426d96b0843ed4e5321ae6cd8a050f82
Avery Khoo <averykhoo@gmail.com>
2021-09-10 23:13:55 +0800

    refactor

--- availability/google_sheets_test.py
+++ availability/google_sheets_test.py
@@ -9,6 +9,8 @@ from googleapiclient.discovery import build
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+
 # Scope (https://developers.google.com/sheets/api/guides/authorizing)
 #   Meaning
 # https://www.googleapis.com/auth/spreadsheets.readonly
@@ -22,19 +24,10 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 # https://www.googleapis.com/auth/drive
 #   Full, permissive scope to access all of a user's files. Request this scope only when it is strictly necessary.
 
-# https://docs.google.com/spreadsheets/d/1ahbAXvuamz2PB1COGx2dWjIV8BN75bqYL_KmgdHkWKk/edit#gid=1211096710
-# spreadsheet_id = '1ahbAXvuamz2PB1COGx2dWjIV8BN75bqYL_KmgdHkWKk'
-
-# https://docs.google.com/spreadsheets/d/1Hx_oFmbRYRuek_eyVUyfz4_b9861mPhSBF1NHH9et70/edit#gid=1116371039
-# spreadsheet_id = '1Hx_oFmbRYRuek_eyVUyfz4_b9861mPhSBF1NHH9et70'  # copy, so I don't break anything
-
-
-# https://docs.google.com/spreadsheets/d/1NeklzsZ_EZXz0W5eyPdRbZmGpNqIdAGJyIMVYO342oo/edit#gid=0
-spreadsheet_id = '1NeklzsZ_EZXz0W5eyPdRbZmGpNqIdAGJyIMVYO342oo'  # random unused sheet
 
 
 class Workbook:
-    def __init__(self):
+    def __init__(self, spreadsheet_id):
 
         """Shows basic usage of the Sheets API.
         Prints values from a sample spreadsheet.
@@ -61,11 +54,19 @@ class Workbook:
 
         service = build('sheets', 'v4', credentials=creds)
         self.sheet = service.spreadsheets()
+        self.spreadsheet_id = spreadsheet_id
+
+    def _get_sheet_id(self, sheet_name):
+        result = self.sheet.get(spreadsheetId=self.spreadsheet_id,
+                                ranges=[],
+                                includeGridData=False,
+                                ).execute()
+        return result['sheets'][0]['properties']['sheetId']
 
     def get_sheet_range_values(self, sheet_name, range_start, range_end):
         assert re.fullmatch(r'[A-Z]+[0-9]+', range_start)
         assert re.fullmatch(r'[A-Z]+[0-9]+', range_end)
-        result = self.sheet.values().get(spreadsheetId=spreadsheet_id,
+        result = self.sheet.values().get(spreadsheetId=self.spreadsheet_id,
                                          range=f'{sheet_name}!{range_start}:{range_end}',
                                          ).execute()
         print(result.get('range'))
@@ -74,7 +75,7 @@ class Workbook:
 
     def get_cell_properties(self, sheet_name, cell_address):
         assert re.fullmatch(r'[A-Z]+[0-9]+', cell_address)
-        result = self.sheet.get(spreadsheetId=spreadsheet_id,
+        result = self.sheet.get(spreadsheetId=self.spreadsheet_id,
                                 ranges=[f'{sheet_name}!{cell_address}'],
                                 includeGridData=True,
                                 ).execute()
@@ -86,19 +87,12 @@ class Workbook:
         }
 
     def set_cell_format(self, sheet_name, cell_row, cell_column, red, green, blue):
-
-        result = self.sheet.get(spreadsheetId=spreadsheet_id,
-                                ranges=[f'{sheet_name}!A1'],
-                                includeGridData=False,
-                                ).execute()
-        pprint(result)
-        sheet_id = result['sheets'][0]['properties']['sheetId']
         body = {
             "requests": [
                 {
                     "updateCells": {
                         "range":  {
-                            "sheetId":          sheet_id,
+                            "sheetId":          self._get_sheet_id(sheet_name),
                             "startRowIndex":    cell_row,
                             "endRowIndex":      cell_row + 1,
                             "startColumnIndex": cell_column,
@@ -124,24 +118,31 @@ class Workbook:
                 }
             ]
         }
-        return self.sheet.batchUpdate(spreadsheetId=spreadsheet_id, body=body).execute()
+        return self.sheet.batchUpdate(spreadsheetId=self.spreadsheet_id, body=body).execute()
 
 
 if __name__ == '__main__':
-    wb = Workbook()
+    # https://docs.google.com/spreadsheets/d/1ahbAXvuamz2PB1COGx2dWjIV8BN75bqYL_KmgdHkWKk/edit#gid=1211096710
+    # wb = Workbook('1ahbAXvuamz2PB1COGx2dWjIV8BN75bqYL_KmgdHkWKk')
+
+    # https://docs.google.com/spreadsheets/d/1Hx_oFmbRYRuek_eyVUyfz4_b9861mPhSBF1NHH9et70/edit#gid=1116371039
+    # wb = Workbook('1Hx_oFmbRYRuek_eyVUyfz4_b9861mPhSBF1NHH9et70')  # copy, so I don't break anything
+
+    # https://docs.google.com/spreadsheets/d/1NeklzsZ_EZXz0W5eyPdRbZmGpNqIdAGJyIMVYO342oo/edit#gid=0
+    wb = Workbook('1NeklzsZ_EZXz0W5eyPdRbZmGpNqIdAGJyIMVYO342oo')  # random unused sheet
 
     # # values = wb.get_sheet_range_values('Blk 95A', 'A1', 'P29')
     # # values = wb.get_sheet_range_values('Blk 95A', 'A11', 'B12')
-    # values = wb.get_sheet_range_values('Sheet1', 'A1', 'B2')
-    # if not values:
-    #     print('No data found.')
-    # else:
-    #     for row in values:
-    #         print(row)
-    #
-    # values = wb.get_cell_properties('Sheet1', 'B2')
-    # # pprint(values['properties'])
-    # pprint(values)
+    values = wb.get_sheet_range_values('Sheet1', 'A1', 'B2')
+    if not values:
+        print('No data found.')
+    else:
+        for row in values:
+            print(row)
+
+    values = wb.get_cell_properties('Sheet1', 'B2')
+    # pprint(values['properties'])
+    pprint(values)
 
     values = wb.set_cell_format('Sheet1', 1, 1, 0.6, 0.6, 0.6)
     pprint(values)
