5c5c869cbd59d3a7d0032af3023df5736e7635d9
Avery Khoo <averykhoo@gmail.com>
2021-09-18 18:39:44 +0800

    nearest color name

--- availability/color_distance.py
+++ availability/color_distance.py
@@ -3,6 +3,158 @@ import math
 
 import numpy as np
 
+colors = {
+    "aliceblue":            (240, 248, 255),
+    "antiquewhite":         (250, 235, 215),
+    "aqua":                 (0, 255, 255),
+    "aquamarine":           (127, 255, 212),
+    "azure":                (240, 255, 255),
+    "beige":                (245, 245, 220),
+    "bisque":               (255, 228, 196),
+    "black":                (0, 0, 0),
+    "blanchedalmond":       (255, 235, 205),
+    "blue":                 (0, 0, 255),
+    "blueviolet":           (138, 43, 226),
+    "brown":                (165, 42, 42),
+    "burlywood":            (222, 184, 135),
+    "cadetblue":            (95, 158, 160),
+    "chartreuse":           (127, 255, 0),
+    "chocolate":            (210, 105, 30),
+    "coral":                (255, 127, 80),
+    "cornflowerblue":       (100, 149, 237),
+    "cornsilk":             (255, 248, 220),
+    "crimson":              (220, 20, 60),
+    "cyan":                 (0, 255, 255),
+    "darkblue":             (0, 0, 139),
+    "darkcyan":             (0, 139, 139),
+    "darkgoldenrod":        (184, 134, 11),
+    "darkgray":             (169, 169, 169),
+    "darkgreen":            (0, 100, 0),
+    "darkgrey":             (169, 169, 169),
+    "darkkhaki":            (189, 183, 107),
+    "darkmagenta":          (139, 0, 139),
+    "darkolivegreen":       (85, 107, 47),
+    "darkorange":           (255, 140, 0),
+    "darkorchid":           (153, 50, 204),
+    "darkred":              (139, 0, 0),
+    "darksalmon":           (233, 150, 122),
+    "darkseagreen":         (143, 188, 143),
+    "darkslateblue":        (72, 61, 139),
+    "darkslategray":        (47, 79, 79),
+    "darkslategrey":        (47, 79, 79),
+    "darkturquoise":        (0, 206, 209),
+    "darkviolet":           (148, 0, 211),
+    "deeppink":             (255, 20, 147),
+    "deepskyblue":          (0, 191, 255),
+    "dimgray":              (105, 105, 105),
+    "dimgrey":              (105, 105, 105),
+    "dodgerblue":           (30, 144, 255),
+    "firebrick":            (178, 34, 34),
+    "floralwhite":          (255, 250, 240),
+    "forestgreen":          (34, 139, 34),
+    "fuchsia":              (255, 0, 255),
+    "gainsboro":            (220, 220, 220),
+    "ghostwhite":           (248, 248, 255),
+    "gold":                 (255, 215, 0),
+    "goldenrod":            (218, 165, 32),
+    "gray":                 (128, 128, 128),
+    "green":                (0, 128, 0),
+    "greenyellow":          (173, 255, 47),
+    "grey":                 (128, 128, 128),
+    "honeydew":             (240, 255, 240),
+    "hotpink":              (255, 105, 180),
+    "indianred":            (205, 92, 92),
+    "indigo":               (75, 0, 130),
+    "ivory":                (255, 255, 240),
+    "khaki":                (240, 230, 140),
+    "lavender":             (230, 230, 250),
+    "lavenderblush":        (255, 240, 245),
+    "lawngreen":            (124, 252, 0),
+    "lemonchiffon":         (255, 250, 205),
+    "lightblue":            (173, 216, 230),
+    "lightcoral":           (240, 128, 128),
+    "lightcyan":            (224, 255, 255),
+    "lightgoldenrodyellow": (250, 250, 210),
+    "lightgray":            (211, 211, 211),
+    "lightgreen":           (144, 238, 144),
+    "lightgrey":            (211, 211, 211),
+    "lightpink":            (255, 182, 193),
+    "lightsalmon":          (255, 160, 122),
+    "lightseagreen":        (32, 178, 170),
+    "lightskyblue":         (135, 206, 250),
+    "lightslategray":       (119, 136, 153),
+    "lightslategrey":       (119, 136, 153),
+    "lightsteelblue":       (176, 196, 222),
+    "lightyellow":          (255, 255, 224),
+    "lime":                 (0, 255, 0),
+    "limegreen":            (50, 205, 50),
+    "linen":                (250, 240, 230),
+    "magenta":              (255, 0, 255),
+    "maroon":               (128, 0, 0),
+    "mediumaquamarine":     (102, 205, 170),
+    "mediumblue":           (0, 0, 205),
+    "mediumorchid":         (186, 85, 211),
+    "mediumpurple":         (147, 112, 219),
+    "mediumseagreen":       (60, 179, 113),
+    "mediumslateblue":      (123, 104, 238),
+    "mediumspringgreen":    (0, 250, 154),
+    "mediumturquoise":      (72, 209, 204),
+    "mediumvioletred":      (199, 21, 133),
+    "midnightblue":         (25, 25, 112),
+    "mintcream":            (245, 255, 250),
+    "mistyrose":            (255, 228, 225),
+    "moccasin":             (255, 228, 181),
+    "navajowhite":          (255, 222, 173),
+    "navy":                 (0, 0, 128),
+    "oldlace":              (253, 245, 230),
+    "olive":                (128, 128, 0),
+    "olivedrab":            (107, 142, 35),
+    "orange":               (255, 165, 0),
+    "orangered":            (255, 69, 0),
+    "orchid":               (218, 112, 214),
+    "palegoldenrod":        (238, 232, 170),
+    "palegreen":            (152, 251, 152),
+    "paleturquoise":        (175, 238, 238),
+    "palevioletred":        (219, 112, 147),
+    "papayawhip":           (255, 239, 213),
+    "peachpuff":            (255, 218, 185),
+    "peru":                 (205, 133, 63),
+    "pink":                 (255, 192, 203),
+    "plum":                 (221, 160, 221),
+    "powderblue":           (176, 224, 230),
+    "purple":               (128, 0, 128),
+    "red":                  (255, 0, 0),
+    "rosybrown":            (188, 143, 143),
+    "royalblue":            (65, 105, 225),
+    "saddlebrown":          (139, 69, 19),
+    "salmon":               (250, 128, 114),
+    "sandybrown":           (244, 164, 96),
+    "seagreen":             (46, 139, 87),
+    "seashell":             (255, 245, 238),
+    "sienna":               (160, 82, 45),
+    "silver":               (192, 192, 192),
+    "skyblue":              (135, 206, 235),
+    "slateblue":            (106, 90, 205),
+    "slategray":            (112, 128, 144),
+    "slategrey":            (112, 128, 144),
+    "snow":                 (255, 250, 250),
+    "springgreen":          (0, 255, 127),
+    "steelblue":            (70, 130, 180),
+    "tan":                  (210, 180, 140),
+    "teal":                 (0, 128, 128),
+    "thistle":              (216, 191, 216),
+    "tomato":               (255, 99, 71),
+    "transparent":          (0, 0, 0),
+    "turquoise":            (64, 224, 208),
+    "violet":               (238, 130, 238),
+    "wheat":                (245, 222, 179),
+    "white":                (255, 255, 255),
+    "whitesmoke":           (245, 245, 245),
+    "yellow":               (255, 255, 0),
+    "yellowgreen":          (154, 205, 50),
+    "rebeccapurple":        (102, 51, 153),
+}
+
 
 # Converts RGB pixel array to XYZ format.
 # Implementation derived from http://www.easyrgb.com/en/math.php
@@ -232,7 +384,12 @@ def ok_distance(rgb_1, rgb_2):
     l1, a1, b1 = srgb_to_oklab(*rgb_1)
     l2, a2, b2 = srgb_to_oklab(*rgb_2)
     # return 400 * ((1 - (1 - (l1 - l2) ** 2) ** 0.5) + (a1 - a2) ** 2 + (b1 - b2) ** 2) ** 0.5
-    return 200 * (((l1 - l2)/2) ** 2 + (a1 - a2) ** 2 + (b1 - b2) ** 2) ** 0.5
+    return 200 * (((l1 - l2) / 2) ** 2 + (a1 - a2) ** 2 + (b1 - b2) ** 2) ** 0.5
+
+
+def nearest_color_name(rgb):
+    dist, color_name = min((ok_distance(rgb, color_rgb), color_name) for color_name, color_rgb in colors.items())
+    return color_name
 
 
 if __name__ == '__main__':
@@ -252,13 +409,13 @@ if __name__ == '__main__':
     aqua_lab = rgb2lab(aqua)
     teal_lab = rgb2lab(teal)
 
-    print('white_lab', white_lab)
-    print('black_lab', black_lab)
-    print('red_lab', red_lab)
-    print('green_lab', green_lab)
-    print('blue_lab', blue_lab)
-    print('aqua_lab', aqua_lab)
-    print('teal_lab', teal_lab)
+    print('white_lab', white_lab, nearest_color_name(white))
+    print('black_lab', black_lab, nearest_color_name(black))
+    print('red_lab', red_lab, nearest_color_name(red))
+    print('green_lab', green_lab, nearest_color_name(green))
+    print('blue_lab', blue_lab, nearest_color_name(blue))
+    print('aqua_lab', aqua_lab, nearest_color_name(aqua))
+    print('teal_lab', teal_lab, nearest_color_name(teal))
 
     print('white_ok', srgb_to_oklab(*white))
     print('black_ok', srgb_to_oklab(*black))
@@ -268,20 +425,20 @@ if __name__ == '__main__':
     print('aqua_ok', srgb_to_oklab(*aqua))
     print('teal_ok', srgb_to_oklab(*teal))
 
-    print('lab white, black', ciede2000(white_lab, black_lab), euclidean(white_lab, black_lab)/2)
-    print('lab blue, red', ciede2000(blue_lab, red_lab), euclidean(blue_lab, red_lab)/2)
-    print('lab red, green', ciede2000(red_lab, green_lab), euclidean(red_lab, green_lab)/2)
-    print('lab blue, green', ciede2000(blue_lab, green_lab), euclidean(blue_lab, green_lab)/2)
+    print('lab white, black', ciede2000(white_lab, black_lab), euclidean(white_lab, black_lab) / 2)
+    print('lab blue, red', ciede2000(blue_lab, red_lab), euclidean(blue_lab, red_lab) / 2)
+    print('lab red, green', ciede2000(red_lab, green_lab), euclidean(red_lab, green_lab) / 2)
+    print('lab blue, green', ciede2000(blue_lab, green_lab), euclidean(blue_lab, green_lab) / 2)
 
-    print('lab blue, aqua', ciede2000(blue_lab, aqua_lab), euclidean(blue_lab, aqua_lab)/2)
-    print('lab blue, teal', ciede2000(blue_lab, teal_lab), euclidean(blue_lab, teal_lab)/2)
-    print('lab teal, aqua', ciede2000(teal_lab, aqua_lab), euclidean(teal_lab, aqua_lab)/2)
+    print('lab blue, aqua', ciede2000(blue_lab, aqua_lab), euclidean(blue_lab, aqua_lab) / 2)
+    print('lab blue, teal', ciede2000(blue_lab, teal_lab), euclidean(blue_lab, teal_lab) / 2)
+    print('lab teal, aqua', ciede2000(teal_lab, aqua_lab), euclidean(teal_lab, aqua_lab) / 2)
 
-    print('ok white, black', euclidean(white, black)/3.6, ok_distance(white, black))
-    print('ok blue, red', euclidean(blue, red)/3.6, ok_distance(blue, red))
-    print('ok red, green', euclidean(red, green)/3.6, ok_distance(red, green))
-    print('ok blue, green', euclidean(blue, green)/3.6, ok_distance(blue, green))
+    print('ok white, black', euclidean(white, black) / 3.6, ok_distance(white, black))
+    print('ok blue, red', euclidean(blue, red) / 3.6, ok_distance(blue, red))
+    print('ok red, green', euclidean(red, green) / 3.6, ok_distance(red, green))
+    print('ok blue, green', euclidean(blue, green) / 3.6, ok_distance(blue, green))
 
-    print('ok blue, aqua', euclidean(blue, aqua)/3.6, ok_distance(blue, aqua))
-    print('ok blue, teal', euclidean(blue, teal)/3.6, ok_distance(blue, teal))
-    print('ok teal, aqua', euclidean(teal, aqua)/3.6, ok_distance(teal, aqua))
+    print('ok blue, aqua', euclidean(blue, aqua) / 3.6, ok_distance(blue, aqua))
+    print('ok blue, teal', euclidean(blue, teal) / 3.6, ok_distance(blue, teal))
+    print('ok teal, aqua', euclidean(teal, aqua) / 3.6, ok_distance(teal, aqua))
