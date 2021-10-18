beb2c3a1336c23904297b35412309a565739210a
Avery Khoo <averykhoo@gmail.com>
2021-10-18 19:52:56 +0800

    fix append dimension

--- availability/google_sheets_test.py
+++ availability/google_sheets_test.py
@@ -5,7 +5,6 @@ import re
 import string
 import time
 from dataclasses import dataclass
-from pprint import pprint
 from typing import Dict
 from typing import List
 from typing import Optional
@@ -15,7 +14,6 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from tabulate import tabulate
 
 from availability.color_distance import nearest_color_name
 
@@ -336,17 +334,23 @@ class Sheet:
                                        body=body).execute()
 
     def append_sheet_dimensions(self, additional_rows, additional_columns):
+        assert additional_rows >= 0
+        assert additional_columns >= 0
+        assert additional_rows or additional_columns
         # https://stackoverflow.com/questions/61791361/how-to-resize-a-worksheet-with-the-google-client-library
-        body = {"requests": [{"appendDimension": {
-            "sheetId":   self._get_sheet_id(),
-            "dimension": "ROWS",
-            "length":    additional_rows
-        }},
-            {"appendDimension": {
+        body = {"requests": []}
+        if additional_rows:
+            body['requests'].append({"appendDimension": {
+                "sheetId":   self._get_sheet_id(),
+                "dimension": "ROWS",
+                "length":    additional_rows
+            }})
+        if additional_columns:
+            body['requests'].append({"appendDimension": {
                 "sheetId":   self._get_sheet_id(),
                 "dimension": "COLUMNS",
                 "length":    additional_columns
-            }}]}
+            }})
         self.sheet_service.batchUpdate(spreadsheetId=self.spreadsheet_id,
                                        body=body).execute()
 
@@ -620,18 +624,19 @@ if __name__ == '__main__':
     #             tables[block][f'#{level:02d}-{stack}'] = build_column_notation(row_idx, col_idx)
     #     print(tables[block])
 
+    # https://docs.google.com/spreadsheets/d/1Hx_oFmbRYRuek_eyVUyfz4_b9861mPhSBF1NHH9et70/edit#gid=982331268
     # sheet = Sheet('1Hx_oFmbRYRuek_eyVUyfz4_b9861mPhSBF1NHH9et70', 'Blk 95A')  # copy
     # colors = sheet.get_background_colors('A15', 'C20')
     # pprint(colors)
     # pprint([[color_hex_to_name(cell) for cell in row] for row in colors])
-    # sheet = Sheet('1Hx_oFmbRYRuek_eyVUyfz4_b9861mPhSBF1NHH9et70', 'Sheet30')  # copy
-    # # sheet.set_sheet_dimensions(5, 5)
-    # # sheet.append_sheet_dimensions(5, 2)
-    # print(sheet.get_sheet_dimensions())
+    sheet = Sheet('1Hx_oFmbRYRuek_eyVUyfz4_b9861mPhSBF1NHH9et70', 'Sheet30')  # copy
+    sheet.set_sheet_dimensions(5, 5)
+    sheet.append_sheet_dimensions(5, 0)
+    print(sheet.get_sheet_dimensions())
 
-    workbook_id = '1ahbAXvuamz2PB1COGx2dWjIV8BN75bqYL_KmgdHkWKk'  # original
-    # workbook_id = '1Hx_oFmbRYRuek_eyVUyfz4_b9861mPhSBF1NHH9et70'  # copy, so I don't break anything
-    sheet = Sheet(workbook_id, 'Units taken by date')
-    next_row = sheet.get_first_empty_row_after_existing_content()
-    cache = SheetCache(sheet.get_values('A1', f'Z{next_row}'))
-    pprint(cache.table)
+    # workbook_id = '1ahbAXvuamz2PB1COGx2dWjIV8BN75bqYL_KmgdHkWKk'  # original
+    # # workbook_id = '1Hx_oFmbRYRuek_eyVUyfz4_b9861mPhSBF1NHH9et70'  # copy, so I don't break anything
+    # sheet = Sheet(workbook_id, 'Units taken by date')
+    # next_row = sheet.get_first_empty_row_after_existing_content()
+    # cache = SheetCache(sheet.get_values('A1', f'Z{next_row}'))
+    # pprint(cache.table)
